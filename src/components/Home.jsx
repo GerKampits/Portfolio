81d186653e298adbab41d5b4a85673c0a281d888
GerKampits <gerkampitsis@gmail.com>
2024-01-10 22:42:47 +0200

    Minor change to the text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <p className='text-[#6efeeb]'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Gerasimos Kampitsis</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a software developer</h2>
-            <p className=' text-[#8892b0] py-4 max-w-[700px]'>As an idividual with a passion for helping 
+            <p className=' text-[#8892b0] py-4 max-w-[700px]'>As an individual with a passion for helping 
             others regain their optimal physical well-being and a background in Health Sciences, 
             I have decided to embark on a new and exciting journey in the realm of Software Development. 
             My experience has instilled in me a deep appreciation for holistic well-being and the importance of 
